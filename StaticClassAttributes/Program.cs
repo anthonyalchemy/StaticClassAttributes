ce375c7eb3fa34f1991ab71073aa5df35c754fee
anthonyalchemy <anthony@alchemytechs.com>
2019-08-01 09:27:38 +0100

    Add project files.

---
+++ StaticClassAttributes/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace StaticClassAttributes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //static attribute contains on class itself instead of the object
+            //normal attribute will have different values for each object 
+            //static attribute will be about the class
+
+            Song holiday = new Song ("Holiday", "Green Day", 200); //duration is in seconds 
+            Console.WriteLine(Song.songCount);//one way of doing static
+            Song kashmir = new Song("Kashmir", "Led Zeppelin", 150);
+            Console.WriteLine(kashmir.getSongCount());//another way of doing it by creating a method in song class 
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ StaticClassAttributes/Song.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StaticClassAttributes
+{
+    class Song
+    {
+        public string title; //normal attributes
+        public string artist;
+        public int duration;
+        public static int songCount = 0;
+
+        public Song (string aTitle, string aArtist, int aDuration)
+        {
+            title = aTitle;
+            artist = aArtist;
+            duration = aDuration; //constructor 
+            songCount++;//so every time a song object gets created the songcount gets incremented
+        }
+
+        public int getSongCount()
+        {
+            return songCount;
+        }
+
+    }
+}
